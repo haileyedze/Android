620a5408200d50f0eaa8c7a653cbdb21af050962
haileyedze <haileyedze@gmail.com>
2022-07-12 17:51:32 +0900

    2022-07-12 First Android Study

---
+++ And01_HelloWorld/app/src/main/java/com/example/and01_helloworld/StudentMain.java
@@ -0,0 +1,14 @@
+package com.example.and01_helloworld;
+
+public class StudentMain {
+    //주석
+    //변수선언
+    int num1, num2;
+
+    public static void main(String[] args) {
+        //num1에 10 담아보기
+        StudentMain sm = new StudentMain();
+        sm.num1 = 10;
+        System.out.println(sm.num1);
+    }//main()
+}//class

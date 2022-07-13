03754905a8fa8a836e60cd67b25ab5eddaaa4af8
haileyedze <haileyedze@gmail.com>
2022-07-13 17:50:49 +0900

    no message

--- And01_HelloWorld/app/src/main/java/com/example/and01_helloworld/MainActivity.java
+++ And01_HelloWorld/app/src/main/java/com/example/and01_helloworld/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.and01_helloworld;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.DragStartHelper;
 
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);//디자인파일과 연결
+
+
     }
 }
